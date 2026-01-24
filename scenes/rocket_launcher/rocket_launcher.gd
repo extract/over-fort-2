ac33c27a708d1dd4de5d2fe9fa8820ef8e9c6854
henrik syvertsson <henrik.syvertsson@gmail.com>
2026-01-24 15:42:49 +0100

    add rocket cooldown

--- scenes/rocket_launcher/rocket_launcher.gd
+++ scenes/rocket_launcher/rocket_launcher.gd
@@ -3,10 +3,11 @@ extends Node3D
 
 @export var action_key = ""
 @export var rocket: PackedScene
-
+@export var cooldown = 1
+var cooldown_left=0
 
 func doAbility():
-	print("rocket launched")
+	cooldown_left = cooldown
 	var inst: Node3D = rocket.instantiate()
 	inst.position = global_position + Vector3(0, 2,0)
 	inst.rotation = global_rotation
@@ -14,6 +15,8 @@ func doAbility():
 	
 
 
-func _process(_delta: float) -> void:
-	if Input.is_action_pressed(action_key):
+func _process(delta: float) -> void:
+	cooldown_left -= delta
+	
+	if Input.is_action_pressed(action_key) and cooldown_left < 0:
 		doAbility()
