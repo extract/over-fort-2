597ebea60947934907c5fdeac8bdec9b16d2d6cb
extract <decodeish@gmail.com>
2026-01-24 14:49:48 +0100

    added player joining map

---
+++ scenes/maps/lobby/player_count_update.gd
@@ -0,0 +1,13 @@
+extends Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var count = len(multiplayer.get_peers()) + 1
+	text = "(" + str(count) + "/3) connected"
+	pass
