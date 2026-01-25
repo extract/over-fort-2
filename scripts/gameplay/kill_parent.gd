3067db85dae20e537aa6b5ca6b7a27f20eb128d1
Etion Pinari <61715561+EtionPinari@users.noreply.github.com>
2026-01-25 11:49:55 +0100

    Created turret & added it to local map

---
+++ scripts/gameplay/kill_parent.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+# This creates a slot in the Inspector window
+@export var health_component: HealthComponent 
+
+func _ready() -> void:
+	if health_component:
+		health_component.died.connect(_on_died)
+	else:
+		push_error("HealthComponent not assigned to " + owner.name)
+
+func _on_died():
+	get_parent().queue_free()
